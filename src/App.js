c469d86631c5187116bc9058f0dc1f7d4062b5aa
Emiko <emi9361@hotmail.co.jp>
2021-03-30 00:42:33 +0900

    added newtype count button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ const App = () => {
   const increment = () => setCount(count + 1);
   const decrement = () => setCount(count - 1);
 
+  const increment2 = () => setCount(previousCount => previousCount + 1);
+  const decrement2 = () => setCount(previousCount => previousCount - 1);
+
   return (
     <>
       <div>count: {count}
@@ -12,6 +15,10 @@ const App = () => {
       <div>
         <button onClick={increment}>BUTTON</button>
         <button onClick={decrement}>BUTTON</button>
+      </div>
+      <div>
+        <button onClick={increment2}>BUTTON</button>
+        <button onClick={decrement2}>BUTTON</button>
       </div>
     </>
   );
