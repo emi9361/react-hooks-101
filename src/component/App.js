c39f897e64e1f45cd21a9948152b53b7446d8c43
Emiko <emi9361@hotmail.co.jp>
2021-04-02 00:26:09 +0900

    create event form and event table

--- src/component/App.js
+++ src/component/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
@@ -10,7 +11,25 @@ const App = () => {
             <label htmlFor='formEventTitle'>タイトル</label>
             <input className='form-control' id='formEventTitle'></input>
           </div>
+
+          <div className='form-group'>
+            <label htmlFor='formEventBody'>ボディー</label>
+            <input className='form-control' id='formEventBody'></input>
+          </div>
+          <button className='btn btn-warning'>イベントを作成する</button>
+          <button className='btn btn-info'>全てのイベントを削除する</button>
         </form>
+        <h4>イベント作成フォーム</h4>
+        <table className='table table-hover'>
+          <thead>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディー</th>
+            <th></th>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
       </div>
     </>
   );
